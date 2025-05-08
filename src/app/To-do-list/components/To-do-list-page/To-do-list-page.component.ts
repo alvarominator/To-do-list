919a06a9f4793981027cafec15c2234462f3bcf6
alvarominator <alvarisimo@hotmail.com>
2025-05-08 12:29:48 +0200

    Cleaning

--- src/app/To-do-list/components/To-do-list-page/To-do-list-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
-
-@Component({
-  selector: 'to-do-list-page',
-  imports: [ButtonModule],
-  templateUrl: './To-do-list-page.component.html',
-})
-export class ToDoListPageComponent { }
